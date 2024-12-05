5f765b36ab68037bca34b31495e01812284478bd
Diana Shafarenko <shafarenko.di@gmail.com>
2024-12-05 22:54:39 +0300

    third

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,7 +16,58 @@ btn.addEventListener('click', () => {
 
 menuLinks.forEach(link => {
   link.addEventListener('click', () => {
-    menuBtn.classList.remove('open'); // Убираем класс "open" у кнопки
-    menu.classList.remove('menu-open'); // Закрываем меню
+    menuBtn.classList.remove('open');
+    menu.classList.remove('menu-open');
   });
 });
+
+const qan = document.querySelector('.questions__items');
+const qanTitles = document.querySelectorAll('.questions__item-caption');
+
+let currentQuestion = null;
+
+qanTitles.forEach(function (qanTitle) {
+  qanTitle.addEventListener('click', function() {
+    const currentItem = qanTitle.parentElement;
+    const currentText = currentItem.querySelector('.questions__item-text');
+
+    if (currentQuestion && currentQuestion !== currentItem) {
+      const currentQuestionText = currentQuestion.querySelector('.questions__item-text');
+      currentQuestion.querySelector('.questions__item-caption').classList.remove('questions__item-caption--active');
+      currentQuestionText.classList.remove('questions__item-text--visible');
+      currentQuestionText.style.maxHeight = null;
+      currentQuestion.style.paddingBottom = null;
+    }
+
+    if (currentQuestion !== currentItem) {
+      currentQuestion = currentItem;
+      qanTitle.classList.add('questions__item-caption--active');
+      currentText.classList.add('questions__item-text--visible');
+      currentItem.style.paddingBottom = '15px';
+      currentText.style.maxHeight = currentText.scrollHeight + 'px';
+    } else {
+      currentQuestion = null;
+      qanTitle.classList.remove('questions__item-caption--active');
+      currentText.classList.remove('questions__item-text--visible');
+      currentText.style.maxHeight = null;
+      currentItem.style.paddingBottom = null;
+    }
+  });
+});
+
+
+var message="";
+function clickIE() {if (document.all) {(message);return false;}}
+function clickNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2) {
+(message);
+return false;}}}
+if (document.layers) {
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS;
+}else{
+document.onmouseup=clickNS;
+document.oncontextmenu=clickIE;
+}
+document.oncontextmenu=new Function("return false");
